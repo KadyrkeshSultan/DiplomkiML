09bd8230d3f7ae4d0324fcdbd6d51ce3769bca34
Sultan Kadyrkesh <sultankadyr@outlook.com>
2019-04-09 19:19:39 +0600

    ProgressBar

---
+++ Colorize/ProgressBarForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Colorize
+{
+    public partial class ProgressBarForm : Form
+    {
+        public ProgressBarForm()
+        {
+            InitializeComponent();
+            progressBar1.Visible = true;
+            progressBar1.Style = ProgressBarStyle.Marquee;
+            progressBar1.MarqueeAnimationSpeed = 30;
+            this.ControlBox = false;
+            this.StartPosition = FormStartPosition.CenterScreen;
+        }
+    }
+}
